a387843afbce80853680d02818facac2da8a1932
premsai-noolu <premsainoolu01@gmail.com>
2023-10-26 11:15:25 +0530

    ep-02-igniting our app

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
-/*const heading=React.createElement(
-    "h1", 
-    {id:"heading"}, 
-    "hello world from React");
-    
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/**
- * 
- * <div id="parent">
- *    <div id="child">
- *        <h1>i'm a h1 tag</h1>
- *        <h2>i'm a h2 tag</h2>
- *    </div>
- *    <div id="child2">
- *         <h1>i'm h1 tag</h1>
- *         <h2>i'm a h2 tag</h2>
- *    </div>
- * 
- * </div>
- * 
- * 
- * 
- * 
- * 
- */
 const parent=React.createElement(
     "div",
     {id:"parent"},[
     React.createElement("div",{id:"child"},[
-    React.createElement("h1",{}, "im a h1 tag"),
-    React.createElement("h2",{}, "im a h2 tag")
+    React.createElement("h1",{}, "This is Namste React🚀"),
+    React.createElement("h2",{}, "fcgvhbjn")
     ]),
     React.createElement("div",{id:"child2"},[
     React.createElement("h1",{}, "im a h1 tag"),
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namsaste react
+# Namsaste react 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimisation 
+- Minification(in production phase)
+- Bundling
+- Compress
+- consistent Hashing
+- code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
